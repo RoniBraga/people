1b90d93d2f3a84bebd31ebd760b09fd26b0a0af4
RoniBraga <sousaronielson@gmail.com>
2025-01-01 17:23:00 -0300

    adicionada funcionalidade de excluir pessoa

--- src/main/java/br/edu/people/controller/PessoaController.java
+++ src/main/java/br/edu/people/controller/PessoaController.java
@@ -4,13 +4,16 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +50,25 @@ public class PessoaController {
         }
         return ResponseEntity.status(HttpStatus.OK).body(ps.get());
     }
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> atualizarPessoa(@PathVariable(value = "id") UUID id, @RequestBody @Valid PessoaRecordDto pessoaRecordDto){
+        Optional<Pessoa> ps = pessoaRepository.findById(id);
 
+        if(ps.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pessoa não existe");
+        }
+        var pessoa = ps.get();
+        BeanUtils.copyProperties(pessoaRecordDto, pessoa);
+        return ResponseEntity.status(HttpStatus.OK).body(pessoaRepository.save(pessoa));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> excluirPessoa(@PathVariable(value = "id") UUID id){
+        Optional<Pessoa> ps = pessoaRepository.findById(id);
+        if(ps.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pessoa não existe");
+        }
+        pessoaRepository.delete(ps.get());
+        return ResponseEntity.status(HttpStatus.OK).body("pessoa excluída com sucesso");
+    }
    
 }
